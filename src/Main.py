8efd821cfc5dd73d62c56532a69ab0c58a055980
bwaszkiewicz <be.waszkiewicz@gmail.com>
2019-05-28 23:35:31 +0200

    Documentation

--- src/Main.py
+++ src/Main.py
@@ -41,7 +41,7 @@ movies = pd.concat([movies, movies.GenreCorrected.str.get_dummies(sep='|')], axi
 print(f'Size of data set: {movies.shape} with dummies')
 
 # split for train and test sets
-moviesTrain, moviesTest = train_test_split(movies[movies.GenreCorrected != ''], random_state=42, test_size=0.20, shuffle=True)
+moviesTrain, moviesTest = train_test_split(movies[movies.GenreCorrected != ''], random_state=42, test_size=0.15, shuffle=True)
 
 # term frequency in plot (how many times it appears in the plot)
 tfip = TfidfVectorizer(stop_words='english', smooth_idf=False, sublinear_tf=False, norm=None, analyzer='word')
@@ -64,9 +64,9 @@ print(f'There are {len(y_test.columns)} columns in y_test')
 
 # Multinomial Naive Bayes Classification
 multinomialNB = OneVsRestClassifier(MultinomialNB(fit_prior=True, class_prior=None))
-multinomialNB.fit(x_train, y_train.crime)
+multinomialNB.fit(x_train, y_train.drama)
 prediction = multinomialNB.predict(x_test)
-print(f'Test accuracy is {sklearn.metrics.accuracy_score(y_test.crime, prediction)}')
+print(f'Test accuracy is {sklearn.metrics.accuracy_score(y_test.drama, prediction)}')
 
 # plt = PlotLearning.plot_learning_curve(estimator=multinomialNB, title="Test",X=x_test, y=y_test.crime, cv=5)
 # plt.show()
