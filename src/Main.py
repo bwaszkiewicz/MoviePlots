fde1696e3fba127c3e7050349024d0c7744786a3
bwaszkiewicz <be.waszkiewicz@gmail.com>
2019-06-01 16:06:13 +0200

    Plots dictionary

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn
-
+import json
 
 # Includes
 
@@ -32,12 +32,21 @@ movies = PlotsCleansing.plot_cleansing(movies)
 moviesPlot = movies[['PlotCorrected']].replace(r'\\n', ' ', regex=True)
 moviesPlot.to_csv('PlotCorrected.csv')
 
-pd.set_option("display.max_colwidth", 10000)
-allPlots = moviesPlot['PlotCorrected'].to_string(index=False)
+plots = moviesPlot.PlotCorrected
+print("Plots count: "+ str(len(plots)))
+plotsString = plots[0]
+for i in range(1, len(plots)):
+    plotsString += " "+plots[i]
+    print("plot nubmer: "+str(i))
 
-wordsDictionary = PlotsWordDictionary.plots_word_dictionary(allPlots.split())
+# print(string)  # single plot
+
+wordsDictionary = PlotsWordDictionary.plots_word_dictionary(plotsString.split())
 wordsDictionary = RemoveStopWords.remove_stop_words_from_dictionary(wordsDictionary)
 print(wordsDictionary)
+
+# with open('file.txt', 'w') as file:
+#     file.write(json.dumps(wordsDictionary))  # use `json.loads` to do the reverse
 
 # text_file = open("Output.txt", "w")
 # text_file.write(wordsDictionary)
